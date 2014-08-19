bd2a4a6451c5dbae4a98c28466ddc9ee151fd0cd
ab75173 <adam.p.barrett@gmail.com>
2014-08-19 17:51:40 -0400

    user login and auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  get '/signup' => 'users#new'
+  get '/signin' => 'sessions#new'
+  delete '/signout' => 'sessions#destroy'
+
+  resources :users
+  resources :sessions
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
