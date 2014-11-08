e4a71fe5963ec136cbf357a9827ed512cac079f5
4S1D2 <salolsy6@gmail.com>
2014-11-08 16:06:38 -0500

    permissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :events
 
   #Add profile photo for user
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
