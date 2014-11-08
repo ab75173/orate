1a7c49a605b6262b452522a2f5e4ef950f85550a
ab75173 <adam.p.barrett@gmail.com>
2014-11-08 13:40:46 -0500

    added image upload for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ActiveRecord::Base
+  belongs_to :user
+
+  #Add image for event
+  has_attached_file :photo, :styles => { :small => "150x150>" }, :url  => "/assets/products/:id/:style/:basename.:extension"
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+
 end
