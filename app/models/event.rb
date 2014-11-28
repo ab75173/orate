db44d61dc2a52735f5fa4f18e2d0fcf6ac208ad8
ab75173 <adam.p.barrett@gmail.com>
2014-11-28 09:43:59 -0500

    two column homepage

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
 
 
   #Add image for event
-  has_attached_file :photo, :styles => { :small => "150x150>" }, :url  => "/assets/products/:id/:style/:basename.:extension"
+  has_attached_file :photo, :styles => { :small => "150x150>" }, :url  => "/assets/products/:id/:style/:basename.:extension", :default_url => "/assets/products/5/original/event-default.jpeg?1415837387"
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
 end
